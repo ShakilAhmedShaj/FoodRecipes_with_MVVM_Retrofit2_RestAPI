085e2f44335bc3007e1d59aafcf2a5a0e3df36d2
SHAJ <shakilahmedshaj@gmail.com>
2019-05-17 23:10:15 +0600

    10. Retrofit interface methods done

--- app/src/main/java/com/t3ch/shaj/foodrecipes_mvvm/requests/RecipeApi.java
+++ app/src/main/java/com/t3ch/shaj/foodrecipes_mvvm/requests/RecipeApi.java
@@ -1,8 +1,31 @@
 package com.t3ch.shaj.foodrecipes_mvvm.requests;
 
+import com.t3ch.shaj.foodrecipes_mvvm.requests.responses.RecipeResponse;
+import com.t3ch.shaj.foodrecipes_mvvm.requests.responses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 /**
  * Created by Shakil Ahmed Shaj on 17-May-19.
  * shakilahmedshaj@gmail.com
  */
-public class RecipeApi {
+public interface RecipeApi {
+
+    // SEARCH
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchRecipe(
+            @Query("key") String key,
+            @Query("q") String query,
+            @Query("page") String page
+    );
+
+    // GET SPECIFIC RECIPE
+    @GET("api/get")
+    Call<RecipeResponse> getRecipe(
+            @Query("key") String key,
+            @Query("rId") String recipe_id
+    );
+
 }
